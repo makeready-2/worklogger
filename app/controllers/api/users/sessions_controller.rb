b89a06f358de7f3ef04f5c448d192d6c60a54b27
makeready <shotsinthedark@gmail.com>
2020-02-15 23:09:54 -0500

    login / logout / register all working

--- app/controllers/api/users/sessions_controller.rb
+++ app/controllers/api/users/sessions_controller.rb
@@ -2,27 +2,19 @@
 
 module Api
   class Users::SessionsController < Devise::SessionsController
-
-    # before_action :configure_sign_in_params, only: [:create]
-
-    # POST /resource/sign_in
     def create
-      user = warden.authenticate!(auth_options)
-      sign_in(resource_name, user)
-      render json: { user: user.serialize }
+      user = warden.authenticate(auth_options)
+      if user
+        sign_in(resource_name, user)
+        render json: { user: user.serialize }
+      else
+        render json: {}, status: :unauthorized
+      end
     end
 
-    # DELETE /resource/sign_out
     def destroy
       Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
       render json: {}
     end
-
-    # protected
-
-    # If you have extra params to permit, append them to the sanitizer.
-    # def configure_sign_in_params
-    #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-    # end
   end
 end
